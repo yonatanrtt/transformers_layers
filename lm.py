dbab36bff44fd3343496485fbe3b7944a4efd87e
yonatanrtt <38919481+yonatanrtt@users.noreply.github.com>
2022-06-27 00:54:09 +0300

    Delete lm.py

--- lm.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModel, AutoConfig, AutoModelForMaskedLM
-
-
-def init(_lm_name="roberta-base"):
-    tokenizer = AutoTokenizer.from_pretrained(_lm_name)
-    config = AutoConfig.from_pretrained(_lm_name)
-    model = AutoModel.from_pretrained(_lm_name, config=config)
-    model_mlm = AutoModelForMaskedLM.from_pretrained(_lm_name, config=config)
-    return tokenizer, config, model, model_mlm
